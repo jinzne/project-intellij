a324e64a0a3c463bdc0530329aa9bd90b496c141
jinzne <hgiangne@gmail.com>
2024-01-05 18:04:08 -0800

    Re-added the printMessage() method

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        printMessage("Hello, world");
+        printMessage("By: Giang");
+    }
+    private static void printMessage(String message)
+    {
+        System.out.println(message);
+    }
+}
